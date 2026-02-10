fba16c0e06848951b26da653d524397e9c37290a
Oscar Eliezar Romero Quintanilla <elizquinta23@gmail.com>
2026-02-10 11:31:38 -0600

    Nuestra primera aplicación Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">¡Bienvenido a Chirper!</h1>
+                    <p class="mt-4 text-base-content/60">Esta es tu nueva aplicación de Laravel. ¡Es hora de hacerla funcionar!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
